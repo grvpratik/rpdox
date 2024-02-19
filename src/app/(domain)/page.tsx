9bf4d8c3c6b8c736b3418bed793705ba7688e58d
pratikgrv <pratikgaurav37@gmail.com>
2024-02-19 16:05:58 +0530

    core :hammer: : initialize

---
+++ src/app/(domain)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@nextui-org/react";
+import React from "react";
+
+const RootPage = () => {
+	return (
+		<div>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
+			tempore ea? Unde facilis aut exercitationem, molestias ducimus tempore,
+			impedit reprehenderit, molestiae obcaecati provident natus recusandae
+			error quam aliquam in ipsam.
+			
+		</div>
+	);
+};
+
+export default RootPage;
